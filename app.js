3456975259f5e645abf641fa9b1d11d7bc2a761f
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-13 09:44:03 +0600

    increase decrease event added

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('I am in a separate file')
+function updateProductNumber(product,isTrue,num) {
+    const inputValue = document.getElementById(product+'-number');
+    let newValue = parseInt(inputValue.value);
+    if (isTrue) {
+        newValue = newValue + 1;
+    }
+    else if (newValue > 0) {
+        newValue = newValue - 1;
+    }
+    inputValue.value = newValue;
+
+    //update product total
+    const caseTotal = document.getElementById(product+'-total');
+    caseTotal.innerText = newValue * num;
+}
+
+//phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateProductNumber('phone',true,1219);
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateProductNumber('phone',false,1219);
+})
+
+//case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case',true,59);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case',false,59);
+})
